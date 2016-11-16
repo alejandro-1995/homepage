6fe515c0511e16ffdae0cbda5fbc67644235b0d8
alejandro <alejandro.aleman.hernandez@gmail.com>
2016-11-16 01:34:30 -0600

    Optimized js

--- public/js/script.js
+++ public/js/script.js
@@ -1,27 +1,29 @@
-$(function() {
-    $(".form").submit(function() {
-      if ($(".search").val().substring(0, 3) == "-g ") {
-        $(this).attr("action", "https://www.google.com.mx/search?q=search");
-        $(".search").val($(".search").val().substring(3, $(".search").val().length));
-        return true;
-      }
-      else if ($(".search").val().substring(0, 3) == "-u ") {
-        window.location.assign("https://" + $(".search").val().substring(3, $(".search").val().length));
-        return false;
-      }
-      else if ($(".search").val().substring(0, 3) == "-w ") {
-        $(this).attr("action", "https://www.google.com.mx/search?q=search");
-        $(".search").val($(".search").val().substring(3, $(".search").val().length) + " site:w3schools.com");
-        return true;
-      }
-      else if ($(".search").val().substring(0, 3) == "-c ") {
-        $(this).attr("action", "https://www.google.com.mx/search?q=search");
-        $(".search").val($(".search").val().substring(3, $(".search").val().length) + " site:cplusplus.com");
-        return true;
-      }
-      else {
-        $(this).attr("action", "https://www.google.com.mx/search?q=search");
-        return true;
-      }
-  });
-});
+window.onload = function() {
+  document.getElementById("form").onsubmit = function() {
+    var form   = document.getElementById("form");
+    var search = document.getElementById("search");
+    var query  = search.value;
+
+    if (query.charAt(0) != "-" || query.charAt(2) != " ") {
+      form.setAttribute("action", "https://www.google.com.mx/search?q=search");
+      return true;
+    }
+
+    var command = query.charAt(1);
+        query   = query.substring(3, query.length);
+
+    if (command == "u") {
+      window.location.assign("https://" + query);
+      return false;
+    }
+
+    if (command == "g") {
+      form.setAttribute("action", "https://www.google.com.mx/search?q=search");
+      search.value = query;
+      return true;
+    }
+
+    alert("command not found");
+    return false;
+  };
+};
